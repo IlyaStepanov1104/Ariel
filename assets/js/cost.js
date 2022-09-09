d83c8a555ee697122aef5d3c9fd3156231c31e3d
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-09-09 13:54:29 +0300

    cost() and GET-запрос с данными

--- assets/js/cost.js
+++ assets/js/cost.js
@@ -1,3 +1,39 @@
+function month(month_name){
+	switch (month_name){
+		case "ЯНВАРЬ":
+			return "01";
+		case "ФЕВРАЛЬ":
+			return "02";
+		case "МАРТ":
+			return "03";
+		case "АПРЕЛЬ":
+			return "04";
+		case "МАЙ":
+			return "05";
+		case "ИЮНЬ":
+			return "06";
+		case "ИЮЛЬ":
+			return "07";
+		case "АВГУСТ":
+			return "08";
+		case "СЕНТЯБРЬ":
+			return "09";
+		case "ОКТЯБРЬ":
+			return "10";
+		case "НОЯБРЬ":
+			return "11";
+		case "ДЕКАБРЬ":
+			return "12";
+	}
+}
+
+function chooseString(choose){
+	let result = "";
+	for (let i = 0; i < choose.length; i++)
+		result = result + choose[i][0] + "," + choose[i][1] + ";";
+	return result;
+}
+
 function cost(){
 	let all = document.getElementsByClassName("time");
 	let tag = document.getElementById("cost");
@@ -10,7 +46,7 @@ function cost(){
 			} else {
 				sum += 450;
 			}
-			choose.push([document.getElementById(`${all[i].parentNode.parentNode.id.split('_')[0]}_${all[i].parentNode.parentNode.id.split('_')[1]}`).textContent, all[i].parentNode.parentNode.parentNode.previousElementSibling.previousElementSibling.textContent, all[i].nextSibling.textContent]);
+			choose.push([document.getElementById(`${all[i].parentNode.parentNode.id.split('_')[0]}_${all[i].parentNode.parentNode.id.split('_')[1]}`).textContent + "." + month(all[i].parentNode.parentNode.parentNode.previousElementSibling.previousElementSibling.textContent), all[i].nextSibling.textContent]);
 		}
 	}
 	if (sum === 0){
@@ -18,8 +54,9 @@ function cost(){
 	} else {
 		tag.innerHTML = "<h1 class=\"calendar-header\">Рассчёт стоимости для работы:</h1>\n";
 		for (let i = 0; i < choose.length; i++) {
-			tag.innerHTML += `<h3 style="font-weight: 500">${choose[i][0]} ${choose[i][1]} в ${choose[i][2]}</h3>`;
+			tag.innerHTML += `<h3 style="font-weight: 500">${choose[i][0]}.2022 в ${choose[i][1]}</h3>`;
 		}
 		tag.innerHTML += `<h2>Общая сумма - ${sum}&#8381</h2>`
+		tag.innerHTML += '<label class="cost"><input type="submit" onclick="window.open(\'..\/..\/reserve.php?sum=' + String(sum) + 'choose=' + chooseString(choose) + '\')"><span>Забронировать</span></label>'
 	}
 }
